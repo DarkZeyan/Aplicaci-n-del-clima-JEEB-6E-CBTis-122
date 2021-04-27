c091cff76ec7bad3c3ce9ec1e6345a7b16989ce0
DarkZeyan <esonic0234@hotmail.com>
2021-04-26 18:44:53 -0600

    Unidades añadidas para Presión,Humedad y Viento. Time Format modificado.

--- components/weather.js
+++ components/weather.js
@@ -15,14 +15,26 @@ const Weather = ({ansWeather}) => {
     
   
     if(!name) return null;
-    const  sr = new Date(sys.sunrise*1000).toLocaleTimeString('es-MX');
-    const  ss = new Date(sys.sunset*1000).toLocaleTimeString('es-MX');
+    
+    const hmsToHm = (hour) =>{
+        const date = new Date(hour*1000);
 
+        var h = date.getHours();
+        var m = date.getMinutes()
+        var ampm = h >= 12 ? 'PM' : 'AM';
+        h = h % 12;
+        h = h ? h : 12; 
+        m = m < 10 ? '0'+m : m;
+        const time = h + ':' + m + ' ' + ampm;
+        return time
+    }
     const iconID = weather[0].icon;
     
     const capitalize =(str) =>{
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
+    const ws  = wind.speed * 3.6;
+
 
     return (
         <View style={
@@ -36,11 +48,11 @@ const Weather = ({ansWeather}) => {
                 <View style={[styleWeather.dataContainer,{marginTop:10}]}>
                     <View style={{flex:3, alignItems:'center'}}>
                         <Image source={require('../assets/icons8-pressure-gauge-96.png')} style={styleWeather.iconImg} />
-                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{main.pressure}</Text>
+                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{main.pressure}hPa</Text>
                         <Image source={require('../assets/icons8-wet-96.png')} style={styleWeather.iconImg} />
-                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{main.humidity}</Text>
+                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{main.humidity}%</Text>
                         <Image source={require('../assets/icons8-wind-96.png')} style={styleWeather.iconImg} />
-                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{wind.speed}</Text>
+                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{ws.toFixed(2)}km/h</Text>
                     </View>
                     <View style={{flex:3, alignItems:'center' }}>
                         <View>
@@ -49,6 +61,8 @@ const Weather = ({ansWeather}) => {
                         <View style={{margin:20}}>
                             <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>Min.{main.temp_min} &#x2103;</Text>
                             <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>Max.{main.temp_max} &#x2103;</Text>
+                            <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>S. térmica:{main.feels_like} &#x2103;</Text>
+                            
                         </View>
                         <View>
                             <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{capitalize(weather[0].description)}</Text>
@@ -56,9 +70,9 @@ const Weather = ({ansWeather}) => {
                     </View>
                     <View style={{flex:3, alignItems:'center'}}>
                         <Image source={require('../assets/icons8-sunrise-96.png')} style={styleWeather.iconImg} />
-                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{sr}</Text>
+                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{hmsToHm(sys.sunrise)}</Text>
                         <Image source={require('../assets/icons8-sunset-96.png')} style={styleWeather.iconImg} />
-                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{ss}</Text>
+                        <Text style={[styleWeather.txtData,styleWeather.txtShadow]}>{hmsToHm(sys.sunset)}</Text>
                     </View>
                 </View>
             </ImageBackground>
